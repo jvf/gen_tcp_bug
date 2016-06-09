0c873bd95885a2dd5282e3fb209b9af15f4e50e0
jensvfischer <jensvfischer@gmail.com>
2016-06-09 12:27:19 +0200

    add client and server modules

---
+++ src/server.erl
@@ -0,0 +1,31 @@
+-module(server).
+
+-export([start/1, accept_loop/1, receive_loop/1]).
+
+-define(PORT, 8091).
+
+start(NOAcceptors) ->
+    ets:new(accepted, [public, named_table]),
+    {ok, ListenSocket} = gen_tcp:listen(?PORT, [{active,once}, {reuseaddr, true}]),
+    [ spawn(?MODULE, accept_loop, [ListenSocket]) || _ <- lists:seq(1, NOAcceptors) ].
+
+accept_loop(LSock) ->
+    %% io:format("starting acceptor~n"),
+    {ok, ASock} = gen_tcp:accept(LSock),
+    {ok, {Ip, Port}} = inet:peername(ASock),
+    %% io:format("connection from ~w:~w~n", [Ip, Port]),
+    ets:insert(accepted, {Port, Ip}),
+    spawn(?MODULE, accept_loop, [LSock]),
+    receive_loop(ASock).
+
+receive_loop(Socket) ->
+    receive
+        {tcp, _S, "Ping"} ->
+            %% io:format("~w received Ping~n", [self()]),
+            ok = gen_tcp:send(Socket, "Pong"),
+            ok = inet:setopts(Socket, [{active, once}]),
+            receive_loop(Socket);
+        Other ->
+            io:format("error: ~p~n", [Other]),
+            ok
+    end.
